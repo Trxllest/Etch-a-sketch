9a96b583a98e6efa929118dd2457d356ebcbfb0e
David Olona <damilear@outlook.com>
2023-07-21 15:24:07 -0400

    Add square logic

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,14 +4,18 @@ function createGrid(size = 16) {
 
     // create rows
     for (let r = 0; r < size; r++) {
-        const row = document.createElement('div').classList.add('row');
-        const grid = document.querySelector('grid');
+        const row = document.createElement('div')
+        row.classList.add('row');
+        const grid = document.querySelector('#grid');
         grid.appendChild(row); // adds row to grid
 
         // create columns in each row
         for (let c = 0; c < size; c++) {
-            const square = document.createElement('div').classList.add('square');
+            const square = document.createElement('div')
+            square.classList.add('square');
             row.appendChild(square); // add square to row
         }
     }
 }
+
+createGrid(16);
