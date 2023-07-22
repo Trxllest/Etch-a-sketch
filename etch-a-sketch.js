8d2e27824640771ed1dcb7aa2e75a17f976cbc0e
David Olona <damilear@outlook.com>
2023-07-22 00:52:53 -0400

    Added Input

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,12 +1,38 @@
-// Create grid fxn
+let squareSize = 8;
+let gridSize = document.querySelector('input');
+const applyInput = document.querySelector('.apply');
 
-function createGrid(size = 16) {
+
+// Create grid fxn
+createGrid(squareSize);
+
+// Select Size 
+gridSize.addEventListener('input', function(e) {
+    console.log(e.target.value);
+    squareSize = e.target.value;
+})
+
+// Apply selected size
+applyInput.addEventListener('click', function() {
+    resetGrid();
+});
+
+// Change square color on hover
+function addSquareEventListeners () {
+    let squares = document.querySelectorAll('.square');
+    for (let square of squares) {
+        square.addEventListener('mouseover', (e) => e.target.style.backgroundColor = 'red');
+    }
+
+}
+
+function createGrid(size = 8) {
+    const grid = document.querySelector('#grid');
 
     // create rows
     for (let r = 0; r < size; r++) {
         const row = document.createElement('div')
         row.classList.add('row');
-        const grid = document.querySelector('#grid');
         grid.appendChild(row); // adds row to grid
 
         // create columns in each row
@@ -16,16 +42,25 @@ function createGrid(size = 16) {
             row.appendChild(square); // add square to row
         }
     }
+
+    addSquareEventListeners();
 }
 
-createGrid(64);
-
-// Change square color on hover
-function colorSquare() {
-    const squares = document.querySelectorAll('.square');
-    for (let square of squares) {
-        square.addEventListener('mouseover', () => square.style.backgroundColor = 'red')
+// Reset Grid
+function resetGrid() {
+    let rows = document.querySelectorAll('.row').length
+    const grid = document.querySelector('#grid');
+    for (let r = rows; r > 0; r--) {
+        grid.removeChild(grid.lastChild);
     }
+    createGrid(squareSize);
 }
 
-colorSquare();
+
+
+
+
+
+
+
+
